5550270f78ab82275bcb03fa50df57ff43a43d28
Sudip <36448052+sudbrl@users.noreply.github.com>
2024-06-22 14:04:39 +0545

    Credit.py

--- Credit.py
+++ Credit.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from imblearn.over_sampling import SMOTE
 import streamlit as st
